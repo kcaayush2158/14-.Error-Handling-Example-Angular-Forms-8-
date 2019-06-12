b38b31dd47952a369e147ecf0d83d8bc13d2797a
aayush kc <kcaayush2158@gmail.com>
2019-06-12 20:58:32 +0545

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tdf';
+  topics =['java','javascript'];
+  topichasError = true;
+  userModel = new User('','rob@gmail.com',2,' default','morning',true);
+submitted=false;
+errorMesage ;
+      constructor(private _enrollmentService :EnrollmentService){}
+
+  validTopic(value){
+    if(value === 'default'){
+      this.topichasError=true;
+      console.log("error is true");
+    }else{
+      this.topichasError=false;
+      console.log("error is false");
+    }
+  }
+
+  onSubmit(){
+this.submitted=true;
+ this._enrollmentService.enroll(this.userModel).subscribe(
+    data=> console.log('success',data),
+    error => this.errorMesage = error.statusText
+ )
+ 
+  }
+}
